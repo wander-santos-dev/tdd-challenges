8a60517416a9ddd1fb890422c054c18d1deeb555
wander-santos-dev <wander.santos.dev@gmail.com>
2023-01-18 17:10:05 -0300

    TDD with simple sum test done

--- array-sum/sum_test.go
+++ array-sum/sum_test.go
@@ -3,12 +3,14 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	wait := 15
+		got := Sum(numbers)
+		wait := 15
 
-	if got != wait {
-		t.Errorf("Got %d, wait %d, given %v", got, wait, numbers)
-	}
+		if got != wait {
+			t.Errorf("Got %d, wait %d, given %v", got, wait, numbers)
+		}
+	})
 }
